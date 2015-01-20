6b408387eedf0ab26915bab49d366488f02d75e8
visualjazz-ZYuan <ZYuan@visualjazz.com.au>
2015-01-20 16:44:53 +1100

    - add condition to check if the content for the meta data is empty or null.

--- Framework.Utilities/Base/TagManager.cs
+++ Framework.Utilities/Base/TagManager.cs
@@ -5,25 +5,31 @@ using Framework.Utilities.OgTagManagers.Interfaces;
 
 namespace Framework.Utilities.OgTagManagers.Base
 {
-    public abstract class TagManager
-    {
-        public virtual IList<HtmlMeta> GetOgTagMeta()
-        {
-            var htmlMetas = new List<HtmlMeta>();
-
-            foreach (PropertyInfo prop in GetType().GetProperties())
+      public abstract class TagManager
+      {
+            public virtual IList<HtmlMeta> GetOgTagMeta()
             {
-                if ((prop.GetValue(this)) != null)
-                {
-                    var htmlMeta = new HtmlMeta();
-                    htmlMeta.Attributes.Add("property", ((IOgTag)prop.GetValue(this)).Property);
-                    htmlMeta.Content = ((IOgTag)prop.GetValue(this)).Content;
+                  var htmlMetas = new List<HtmlMeta>();
 
-                    htmlMetas.Add(htmlMeta);
-                }
+                  foreach (PropertyInfo prop in GetType().GetProperties())
+                  {
+                        if ((prop.GetValue(this)) != null)
+                        {
+                              var htmlMeta = new HtmlMeta();
+
+                              var content = ((IOgTag)prop.GetValue(this)).Content;
+
+                              if (!string.IsNullOrEmpty(content))
+                              {
+                                    htmlMeta.Attributes.Add("property", ((IOgTag)prop.GetValue(this)).Property);
+                                    htmlMeta.Content = content;
+                              }
+
+                              htmlMetas.Add(htmlMeta);
+                        }
+                  }
+
+                  return htmlMetas;
             }
-
-            return htmlMetas;
-        }
-    }
+      }
 }
