2042bde1bbb95d6eca30f80ef33bcca53268942e
visualjazz-ZYuan <ZYuan@visualjazz.com.au>
2015-01-22 16:30:17 +1100

    Update NuGet package

--- Framework.Website/Demo.aspx.cs
+++ Framework.Website/Demo.aspx.cs
@@ -23,7 +23,7 @@ namespace Framework.Website
                         new OgUrl { Content = "Url goes here" },
                         new OgDescription { Content = "Description goes here" },
                         new OgSiteName { Content = "Site name goes here" },
-                        new OgType {"website"},
+                        new OgType { Content = "website" },
                         imageTagManager
                     );
 
